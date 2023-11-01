90704b099539be5069077742912de52bab7fd788
BESTrOFF <bystrov99.99@mail.ru>
2023-11-01 22:28:14 +0300

    Создал сервлет боя и класс врага

---
+++ src/main/java/com/example/quest/controllers/FightServlet.java
@@ -0,0 +1,27 @@
+package com.example.quest.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "fight", value = "/fight")
+public class FightServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
---
+++ src/main/java/com/example/quest/entity/Enemy.java
@@ -0,0 +1,4 @@
+package com.example.quest.entity;
+
+public class Enemy {
+}
