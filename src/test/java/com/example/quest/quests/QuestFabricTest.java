11f92aefbdc426c1d243ffb60b800d64edf186d7
BESTrOFF <bystrov99.99@mail.ru>
2023-11-03 20:23:31 +0300

    Полностью затестил QuestFabric

---
+++ src/test/java/com/example/quest/quests/QuestFabricTest.java
@@ -0,0 +1,28 @@
+package com.example.quest.quests;
+
+import com.example.quest.exceptions.CreateQuestException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestFabricTest {
+    private static QuestFabric questFabric;
+
+    @BeforeAll
+    public static void init(){
+        questFabric = new QuestFabric();
+    }
+
+    @Test
+    public void createQuest_method_when_incorrect_questName_throw_CreateQuestException(){
+        assertThrows(CreateQuestException.class, () -> {
+            questFabric.createQuest("WrongOrIncorrectName");
+        });
+    }
+
+    @Test
+    public void createQuest_method_create_AlienQuest_when_questName_is_AlienQuest(){
+        assertInstanceOf(AlienQuest.class, questFabric.createQuest("AlienQuest"));
+    }
+}
